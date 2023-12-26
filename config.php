24e9d618b506cc0c00ecc53960242b6ea2245d92
Sudeep S <sudeep.sangamam@gmail.com>
2023-12-26 11:24:32 +0530

    Code Rebuild
    Change of the Key to Space

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 $keyId = 'rzp_live_KEY HERE';
 $keySecret = 'SECRET HERE';
 $displayCurrency = 'INR';
-$key= 'JkOkve0aIlIouhfFjc71uGZsJ36sfCNk';
+$key= 'Key From Resellerclub Goes Here';
 error_reporting(E_ALL);
 ini_set('display_errors', 0);
